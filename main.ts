6f70581c553829ec6504bb53d331f557450b44db
HinaNisar <160288150+HINANISARGITHUB@users.noreply.github.com>
2024-05-14 06:06:05 +0500

    Using this activity you will count the words.

---
+++ main.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+const answer: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: (chalk.bold.magentaBright("Enter your sentence here to count the word: ")),
+    }
+])
+
+const word = answer.Sentence.trim().split(" ")
+console.log(chalk.cyanBright(word));
+console.log(chalk.bold.yellowBright(`The word count of your sentence is: ${word.length}`));
